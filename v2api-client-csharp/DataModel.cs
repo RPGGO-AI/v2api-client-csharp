df22755e3d7e7bf3dda048932a9592a27d43fba5
codingtmd <fisher.lei@gmail.com>
2024-10-23 16:40:54 +0800

    patch 1

---
+++ v2api-client-csharp/DataModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2api_client_csharp
+{
+
+    public class GameMetadataResponse
+    {
+        public int Code { get; set; }
+        public string Msg { get; set; }
+        public GameData Data { get; set; }
+    }
+
+    public class GameData
+    {
+        public string Name { get; set; }
+        public string GameId { get; set; }
+        public string Background { get; set; }
+        public string Intro { get; set; }
+        public string Image { get; set; }
+        public string Genre { get; set; }
+        public List<Chapter> Chapters { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class Chapter
+    {
+        public string Name { get; set; }
+        public string ChapterId { get; set; }
+        public string Background { get; set; }
+        public string Intro { get; set; }
+        public string Image { get; set; }
+        public string BackgroundAudio { get; set; }
+        public string EndingAudio { get; set; }
+    }
+
+
+}
---
+++ v2api-client-csharp/RPGGOClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace v2api_client_csharp
+{
+    public class RPGGOClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public RPGGOClient(string apiKey)
+        {
+            _httpClient = new HttpClient();
+            //_httpClient.DefaultRequestHeaders.Add("accept", "application/json");
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("Authorization", apiKey);
+            //_httpClient.DefaultRequestHeaders..Add("Content-Type", "application/json");
+        }
+
+        public async Task<GameMetadataResponse> GetGameMetadataAsync(string gameId)
+        {
+            var requestBody = new
+            {
+                game_id = gameId
+            };
+
+            var requestContent = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://api.rpggo.ai/v2/open/game/gamemetadata", requestContent);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var gameMetadataResponse = JsonConvert.DeserializeObject<GameMetadataResponse>(responseContent);
+
+            return gameMetadataResponse;
+        }
+    }
+}
